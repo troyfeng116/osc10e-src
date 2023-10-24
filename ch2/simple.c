a531ff1e8b8c5351e816c0375fab52b58808efa1
Troy Feng <troyfeng1@gmail.com>
2023-10-23 20:53:38 -0600

    Add kernel logs to simple kernel module

--- ch2/simple.c
+++ ch2/simple.c
@@ -1,8 +1,8 @@
 /**
  * simple.c
  *
- * A simple kernel module. 
- * 
+ * A simple kernel module.
+ *
  * To compile, run makefile by entering "make"
  *
  * Operating System Concepts - 10th Edition
@@ -13,24 +13,34 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+#include <linux/hash.h>
+#include <linux/gcd.h>
+#include <asm/param.h>
+#include <linux/jiffies.h>
+
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
+    printk(KERN_INFO "Loading Module\n");
 
-       return 0;
+    printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
+    printk(KERN_INFO "jiffies = %lu, HZ = %d\n", jiffies, HZ);
+
+    return 0;
 }
 
 /* This function is called when the module is removed. */
-void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+void simple_exit(void)
+{
+    printk(KERN_INFO "Removing Module\n");
+    printk(KERN_INFO "%lu\n", gcd(3300UL, 24UL));
+    printk(KERN_INFO "jiffies = %lu\n", jiffies);
 }
 
 /* Macros for registering module entry and exit points. */
-module_init( simple_init );
-module_exit( simple_exit );
+module_init(simple_init);
+module_exit(simple_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
 MODULE_AUTHOR("SGG");
-
